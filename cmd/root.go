b9b8a23d20422fc3eb45dffa11ffd57d161e8d18
Brett Bender <brett@brettb.xyz>
2026-03-13 22:56:07 -0500

    bump version

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,7 @@ var rootCmd = &cobra.Command{
 	Use:     "qcd <command> [<arguments>]",
 	Short:   "A quick and easy way to defend during CCDC",
 	Long:    "Quick CCDC Defender is a command line utility that allows you to quickly defend your system.",
-	Version: "0.0.2",
+	Version: "0.1.0",
 }
 
 func Execute() {
